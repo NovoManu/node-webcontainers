5e4332e16825a33ad4768a50f49e039f4378a7cb
Manu Ustenko <ustenko.mi@gmail.com>
2023-04-04 17:18:45 +0200

    Add webcontainers

--- index.js
+++ index.js
@@ -6,8 +6,50 @@ const textarea = document.querySelector('textarea')
 
 let webcontainersInstance
 
+async function installDependencies() {
+    const installProcess = await webcontainersInstance.spawn('npm', ['install'])
+
+    installProcess.output.pipeTo(new WritableStream({
+        write(data) {
+           console.log(data)
+        }
+    }))
+
+    return installProcess.exit
+}
+
+async function startDevServer() {
+    const serverProcess = await webcontainersInstance.spawn('npm', ['run', 'start'])
+
+    serverProcess.output.pipeTo(new WritableStream({
+          write(data) {
+              console.log(data)
+          }
+      }))
+
+    webcontainersInstance.on('server-ready', (port, url) => {
+        console.log(port, url)
+        iframe.src = url
+    })
+}
+
+async function writeIndexJS(file, content) {
+    await webcontainersInstance.fs.writeFile(`/${file}`, content)
+}
+
 window.addEventListener('load', async () => {
     textarea.value = files['index.js'].file.contents
+
+    textarea.addEventListener('input', (e) => {
+        writeIndexJS('index.js', e.currentTarget.value)
+    })
+
+    webcontainersInstance = await WebContainer.boot()
+    await webcontainersInstance.mount(files)
+
+    await installDependencies()
+
+    startDevServer()
 
     console.log('Window is loaded')
 })
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default {
+    server: {
+        port: 5173,
+        headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin',
+        },
+    }
+}
